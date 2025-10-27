0d737f74197cf9695b6baf61c64ce809dd2a030e
Gustavo Dickmann <BRMOU066549@ambev.com.br>
2025-10-27 09:51:29 -0300

    todo: ler nome dos veradores corretamente (site oficial pode ter nome completo, .pdf de presença por ter o nome incompleto. usar algoritmo de fuzzing)

--- leitor-relatorio-presenca.py
+++ leitor-relatorio-presenca.py
@@ -14,7 +14,7 @@ class Councilour(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name: Mapped[str] = mapped_column(sqlalchemy.String, nullable=False)
-    phone: Mapped[str] = mapped_column(sqlalchemy.String, nullable=False)
+    phone: Mapped[str] = mapped_column(sqlalchemy.String, nullable=True)
     email: Mapped[str] = mapped_column(sqlalchemy.String, nullable=False)
     photo_url: Mapped[str] = mapped_column(sqlalchemy.String, nullable=False)
     party: Mapped[str] = mapped_column(sqlalchemy.String, nullable=False)
@@ -26,7 +26,7 @@ class Attendence(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     councilor_id: Mapped[uuid.UUID] = mapped_column(sqlalchemy.ForeignKey("councilours.id"), nullable=False)
-    month: Mapped[date] = mapped_column(default=date.today)
+    month: Mapped[str] = mapped_column(default=date.today)
     status: Mapped[str] = mapped_column(nullable=False)
 
     councilour: Mapped["Councilour"] = relationship(back_populates="attendances")
@@ -38,17 +38,34 @@ def get_attendence_status_from_scrapped_str(text: str):
 def get_name_from_scrapped_str(text: str):
     return re.sub(r"\b(PRESENTE|Ausente|Justificado)\b", "", text, re.IGNORECASE).strip()
 
-def add_attendence(attendences: list[Attendence], text: list[str], last_month: str):
+def add_attendence(client: sqlalchemy.Engine, attendences: list[Attendence], text: list[str]):
     session_date_regex = r"\d{2} de [A-Z][a-z]+ de \d{4}"
     session_date: str
+
+    councilours = get_all_councilours(client)
 
     for i in text:
         if re.match(session_date_regex, i): # will always hit this condition on the first iteration
             session_date = i
             continue
         if any(x in i for x in ['PRESENTE', 'Ausente', 'Justificado']):
-            attendences.append(Attendence(status=get_attendence_status_from_scrapped_str(i)))
-            continue
+            councilour = next((c for c in councilours if c.name == get_name_from_scrapped_str(i)), None)
+
+            if (councilour is None):
+                print(f'O vereador {get_name_from_scrapped_str(i)} participou da reunião de {session_date}, mas ele não foi encontrado '
+                      'na base de dados do paecirco.org.')
+                continue
+
+            attendences.append(Attendence(
+                month=session_date,
+                status=get_attendence_status_from_scrapped_str(i),
+                councilor_id=councilour.id
+            ))
+
+def get_all_councilours(client: sqlalchemy.Engine):
+    with Session(client) as session:
+        stmt = sqlalchemy.select(Councilour)
+        return session.scalars(stmt).all()
 
 def throw_exception_if_current_month_already_executed(client: sqlalchemy.Engine):
     with Session(client) as session:
@@ -58,6 +75,13 @@ def throw_exception_if_current_month_already_executed(client: sqlalchemy.Engine)
         for a in results:
             print(f"ID: {a.id}, Councilor ID: {a.councilor_id}, Month: {a.month}, Status: {a.status}")
 
+def get_councilour_by_name(client: sqlalchemy.Engine, name: str):
+    with Session(client) as session:
+            stmt = sqlalchemy.select(Councilour).where(Councilour.name == name)
+            return session.scalars(stmt).first()
+
+## algorithm start
+
 client = sqlalchemy.create_engine(
     "postgresql+psycopg2://postgres:postgres@localhost:5432/paoecirco.org",
     echo=True
@@ -65,7 +89,9 @@ client = sqlalchemy.create_engine(
 
 Base.metadata.create_all(client)
 
-throw_exception_if_current_month_already_executed(client)
+## TODO os vereadores devem ser adicionados manualmente por script
+
+##throw_exception_if_current_month_already_executed(client)
 
 today = date.today()
 last_month = f"{today.year}/{today.month - 1}/{today.day}" 
@@ -82,4 +108,10 @@ attendences = []
 for i in range(len(reader.pages)):
     page = reader.pages[i]
     text = page.extract_text().splitlines()
-    add_attendence(attendences, text, last_month)
+    add_attendence(client, attendences, text)
+
+with Session(client) as session:
+    print('Iniciando inserção das presenças/ausências das reuniões.')
+    session.add_all(attendences)
+    session.commit()
+    print('Inserção das presenças/ausências das reuniões concluída.')
